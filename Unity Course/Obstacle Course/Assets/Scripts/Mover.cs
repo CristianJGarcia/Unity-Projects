b4f6ba9a3aea3a6a876eca84e31c10b9756c5325
Cristian <garcia.cristian@outlook.com>
2021-08-27 01:38:24 -0500

    added walls , cinemachine camera to follow player

--- Unity Course/Obstacle Course/Assets/Scripts/Mover.cs
+++ Unity Course/Obstacle Course/Assets/Scripts/Mover.cs
@@ -9,17 +9,30 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
         // A and D
-        // Multiply by Time.deltaTime to achieve frame rate independent 
-        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; 
+        // Multiply by Time.deltaTime to achieve frame rate independence 
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        
         // W and S
-        // Multiply by Time.deltaTime to achieve frame rate independent 
+        // Multiply by Time.deltaTime to achieve frame rate independence 
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         
         // Foundation concept of moving the player
